2007913f03afaedb9a2a7e1c76d6a179140f9453
liao <17608478306@163.com>
2025-06-28 10:28:14 +0800

    feat

--- src/api/messageApi.js
+++ src/api/messageApi.js
@@ -3,38 +3,37 @@ import http from '../utils/http'
 const messageApi = {
     // message相关
     // 获取私聊消息列表
-    getPrivateMessageList(miniId){
+    getPrivateMessageList(miniId) {
         return http.GET(`/message-module/api/message/PRIVATE_MESSAGE/loadMessage/${miniId}`);
     },
 
     // 获取群聊消息列表
-    getGroupMessageList(miniId){
+    getGroupMessageList(miniId) {
         return http.GET(`/message-module/api/message/GROUP_MESSAGE/loadMessage/${miniId}`);
     },
 
     // 获取历史消息
-    getHistoryMessage(chatType,param){
-        let type = chatType == "private" ? 0 : 1;
-        return http.GET(`/message-module/api/message/${type}/history`,param);
+    getHistoryMessage(chatType, param) {
+        chatType = chatType.toUpperCase() + "_MESSAGE";
+        return http.GET(`/message-module/api/message/${chatType}/history`, param);
     },
 
     // 发送消息
-    sendMessage(chatType,param){
-        debugger
-        let type = chatType == "private" ? 0 : 1;
-        return http.POST(`/message-module/api/message/${type}/send`,param);
+    sendMessage(chatType, param) {
+        chatType = chatType.toUpperCase() + "_MESSAGE";
+        return http.POST(`/message-module/api/message/${chatType}/send`, param);
     },
 
     // 撤回消息
-    recallMessage(chatType,msgId){
-        let type = chatType == "private" ? 0 : 1;
-        return http.POST(`/message-module/api/message/${type}/recall/${msgId}`);
+    recallMessage(chatType, msgId) {
+        chatType = chatType.toUpperCase() + "_MESSAGE";
+        return http.POST(`/message-module/api/message/${chatType}/recall/${msgId}`);
     },
 
     // 已读消息
-    readedMessage(chatType,targetId){
-        let type = chatType == "private" ? 0 : 1;
-        return http.POST(`/message-module/api/message/${type}/readed?targetId=${targetId}`);
+    readedMessage(chatType, targetId) {
+        chatType = chatType.toUpperCase() + "_MESSAGE";
+        return http.POST(`/message-module/api/message/${chatType}/readed?targetId=${targetId}`);
     }
 };
 
